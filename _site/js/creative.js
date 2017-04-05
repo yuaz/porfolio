de4267ce3e9d56c98b4a1aadd914fe2fbd94f44d
yuaz <y2299zha@uwaterloo.ca>
2017-04-05 09:08:24 -0400

    changed nav bar to disable scroll

--- _site/js/creative.js
+++ _site/js/creative.js
@@ -3,7 +3,6 @@
 
     // jQuery for page scrolling feature - requires jQuery Easing plugin
     $('a.page-scroll').bind('click', function(event) {
-        console.log('Hi')
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: ($($anchor.attr('href')).offset().top - 50)
--- _site/js/myfunctions.js
+++ _site/js/myfunctions.js
@@ -1,18 +1,14 @@
-(function($) {
-    "use strict"; // Start of use strict
+function myChange(){
 
-    // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
-        event.preventDefault();
-    });
+}
 
-    // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function() {
-        $('.navbar-toggle:visible').click();
-    });
+$('a.page-scroll').bind('click', function(event) {
+    var $anchor = $(this);
+    console.log($anchor.attr('href'))
+    $('html, body').stop().animate({
+        scrollTop: ($($anchor.attr('href')).offset().top - 50)
+    }, 1250, 'easeInOutExpo');
+    event.preventDefault();
+});
 
-})(jQuery); // End of use strict
+console.log('This one!')
