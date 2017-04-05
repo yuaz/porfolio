8f7df4700749cfd499b80ae78f762ff67d315961
yuaz <y2299zha@uwaterloo.ca>
2017-04-05 01:21:21 -0400

    update

--- js/myfunctions.js
+++ js/myfunctions.js
@@ -1,13 +1,18 @@
-// function find_url(id){
-//   if ((window.location.href).indexOf('index') >= 0){
-//     // onclick="find_url('about'); return false;"
-//     window.location.href = "#" + id;
-//   } else {
-//     window.location.href = "index.html#" + id;
-//   }
-// }
+(function($) {
+    "use strict"; // Start of use strict
 
-$(document).ready(function() {
-  // console.log('working')
-  // $('#navigation').load('index.html');
-});
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+})(jQuery); // End of use strict
