1cd2114b19308c27f18c4069793cb0486ac3f0e9
yuaz <y2299zha@uwaterloo.ca>
2017-04-05 01:23:16 -0400

    Find error

--- _site/js/creative.js
+++ _site/js/creative.js
@@ -3,6 +3,7 @@
 
     // jQuery for page scrolling feature - requires jQuery Easing plugin
     $('a.page-scroll').bind('click', function(event) {
+        console.log('Hi')
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: ($($anchor.attr('href')).offset().top - 50)
